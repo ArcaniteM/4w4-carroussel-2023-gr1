92e2db4a18a7814c8b5ba5dd8ce83384fbab8bdd
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-04-20 10:31:12 -0400

    lab 5 push du prof

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,94 +1,101 @@
 (function(){
-    //console.log('début du carrousel');
-    let boutcarrousel__ouvrir = document.querySelector('.carrousel__ouvrir')
-    /* ---------------- variable du carroussel */
-    let carrousel = document.querySelector('.carrousel')
-    let carrousel__x = document.querySelector('.carrousel__x')
-    let carrousel__figure = document.querySelector(".carrousel__figure")
-    let carrousel__form = document.querySelector(".carrousel__form")
-
-    /*--------------------- variable de la galerie */
-    let galerie = document.querySelector(".galerie")
-    let galerie__img = galerie.querySelectorAll("img")
-    //console.log("galerie--img " + galerie__img.length);
-    /* ------------------------ ouvrir boite modele */
-    let index = 0
-    let ancien_index = -1
-    let position = 0 //permet d indexer les images de la galerie 
-
-    boutcarrousel__ouvrir.addEventListener('mousedown', function(){
-        carrousel.classList.add('carrousel--ouvrir')
-        ajouter_img_dans_carroussel()
-
-    })
-    carrousel__x.addEventListener('mousedown', function(){
-        carrousel.classList.remove('carrousel--ouvrir')
-    })
-
-    function ajouter_img_dans_carroussel(){
-        
-        for(const elm of galerie__img){
-            elm.dataset.index = position 
-            elm.addEventListener('mousedown', function(){
-                index = this.dataset.index
-                afficher_image();
-            })
-
-            creation_img_carrousel(elm)
-            creation_radio_carrousel()
-        }
-    }
-
-    function creation_img_carrousel(elm){
-            //console.log(elm.getAttribute('src'));
-            let img = document.createElement('img');
-            img.src=elm.src
-            img.classList.add('carrousel__img')
-            //img.setAttribute('src', elm.getAttribute('src'));
-            //console.log(img.getAttribute('src'));
-            carrousel__figure.appendChild(img)
-    }
-
-    /**
-     * creation d un radio noutton
-     */
-
-
-    function creation_radio_carrousel(){
-        let rad = document.createElement('input')
-        rad.setAttribute('type', 'radio')
-        rad.setAttribute('name', 'carrousel__rad')
-        rad.classList.add('carrousel__rad')
-        rad.dataset.index = position
-        position = position + 1
-        carrousel__form.appendChild(rad)
-
-        rad.addEventListener('mousedown', function(){
-            //console.log(this.dataset.index)
-            // carrousel__figure.children[this.dataset.index].style.opacity = 1
-            index = this.dataset.index
-            afficher_image()
-
-        })
-    }
-
-    function afficher_image(){
-
-           if(ancien_index!= -1){
-              // carrousel__figure.children[ancien_index].style.opacity = 0
-               carrousel__figure.children[ancien_index].classList.remove('carrousel__img--activer')
-              //carrousel__form.children[ancient_index].checked = false
-            }
-           carrousel__figure.children[index].classList.add('carrousel__img--activer')
-           //carrousel__figure.children[index].style.opacity = 1
-           ancien_index = index
-    }
-
-    /**
-     * permet de verifier si carrousel--activer se trouve dans la liste de classes 
-     * Carrousel.classList.contain('carrousel--activer')
-     * carrousel.classList.remove('carrousel--activer')
-     */
-
-})() //fonction auto-executante
-
+    console.log('Début du carrousel')
+   let bouton = document.querySelector(".carrousel__ouvrir")
+   /* -------------------------------------------------------- Variable du carrousel */
+   let carrousel  = document.querySelector(".carrousel")
+   let carrousel__x = document.querySelector(".carrousel__x")
+   let carrousel__figure = document.querySelector(".carrousel__figure")
+   let carrousel__form = document.querySelector(".carrousel__form")
+   console.log(carrousel__form.tagName)
+   /* -------------------------------------------------------- Variable de la galerie */
+   let galerie = document.querySelector(".galerie")
+   let galerie__img = galerie.querySelectorAll("img")
+   // console.log("galerie__img: " + galerie__img.length)
+   // console.log(carrousel.tagName)
+   /* ------------------------------------------ positionnement de l'image active du carrousel */
+   let index = 0
+   let ancien_index = -1
+   let position = 0 // permet d'indexer les image de la galerie et 
+   
+   /* ----------------------------------------------------  ouvrir boîte modale */
+   bouton.addEventListener('mousedown', function(){
+       console.log('ouvrir la boîte modale')
+       carrousel.classList.add('carrousel--activer')
+       ajouter_img_dans_carrousel()
+   
+   })
+   /* ----------------------------------------------------  fermer boîte modale */
+   carrousel__x.addEventListener('mousedown', function(){
+       console.log('fermer la boîte modale')
+       carrousel.classList.remove('carrousel--activer')
+   
+   })
+   
+   
+   
+   /** 
+    * ajouter_img_dans_carrousel
+    * Ajouter l'ensemble des images de la galerie dans la boîte modale carrousel
+    */
+   function ajouter_img_dans_carrousel()
+   {
+     
+     for (const elm of galerie__img)
+     {
+       elm.dataset.index = position
+       elm.addEventListener('mousedown',function(){
+         index = this.dataset.index
+         afficher_image(index)
+         console.log(index)
+       })
+   
+       creation_img_carrousel(elm)
+       creation_radio_carrousel()
+     }
+   }
+   
+   function creation_img_carrousel(elm){
+         //console.log(elm.getAttribute('src'))
+         let img = document.createElement('img')
+         // img.setAttribute('src', elm.getAttribute('src'))
+          img.src = elm.src
+          img.classList.add('carrousel__img')
+          //console.log (img.getAttribute('src'))
+          carrousel__figure.appendChild(img)
+   }
+   /**
+    * Création d'un radio-bouton
+    */
+   
+   
+   function  creation_radio_carrousel(){
+     let rad = document.createElement('input')
+     console.log(rad.tagName)
+     rad.setAttribute('type','radio')
+     rad.setAttribute('name', 'carrousel__rad')
+     rad.classList.add('carrousel__rad')
+     rad.dataset.index = position
+     position = position + 1 // incrémentation de 1
+     // position += 1
+     // position++
+     carrousel__form.appendChild(rad)
+     rad.addEventListener('mousedown', function(){
+       console.log(this.dataset.index)
+       index = this.dataset.index
+       afficher_image(index)
+     })
+   }
+   
+   function afficher_image(index){
+   
+     if (ancien_index != -1){
+      // carrousel__figure.children[ancien_index].style.opacity = 0  
+      carrousel__figure.children[ancien_index].classList.remove('carrousel__img--activer')
+     }
+     // carrousel__figure.children[index].style.opacity = 1
+     carrousel__figure.children[index].classList.add('carrousel__img--activer')
+     ancien_index = index
+   }
+   
+   
+   })()
